700644ab7639e09dcc618932e161602145e0f245
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-20 19:18:15 +0300

    create basic get and post routes

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get All Items';
+  }
+
+  @Post()
+  create(): string {
+    return 'Create item';
+  }
+}
