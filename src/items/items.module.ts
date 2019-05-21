6fe09810affc6d76fe59c9e9b0c8c352058f4e3e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-21 17:27:24 +0300

    setup item schema

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,11 @@
+import { ItemSchema } from './schemas/item.schema';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'item', schema: ItemSchema }])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
