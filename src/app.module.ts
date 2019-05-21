0f5a06a97b3ea01e228fddbbb78a9c77d9b8563e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-21 16:57:51 +0300

    import items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
+import { ItemsModule } from './items/items.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ItemsController],
+  imports: [ItemsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
