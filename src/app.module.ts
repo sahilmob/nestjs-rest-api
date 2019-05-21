2861c28f96add1f5870ff960b9e11ba342a79f55
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-21 17:19:46 +0300

    configure mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import keys from './config/keys';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, MongooseModule.forRoot(keys.mongoURI)],
   controllers: [AppController],
   providers: [AppService],
 })
